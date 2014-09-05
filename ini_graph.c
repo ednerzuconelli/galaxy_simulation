c4e50d3d1b1e866503b36161ced2f021493f4ca1
Willians <willians@willians-T68D.(none)>
2014-09-04 21:40:53 -0300

    Ini_graphs de merda...

---
+++ ini_graph.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<graphics.h>
+
+void opengraph(void){
+
+int driver, mode;
+
+detectgraph(&driver, &mode);
+initgraph(&driver, &mode, "");
+
+}
+
+void main(void){
+
+int opcao, driver, mode;
+char *GDrivers[] = {"DETECT",
+		    "CGA",
+		    "MCGA",
+		    "EGA",
+		    "EGA64",
+		    "EGAMONO",
+		    "IBM8514",
+		    "HERCMONO",
+		    "ATT400",
+		    "VGA",
+		    "PC3270"};
+
+opengraph();
+
+detectgraph(&driver, &mode);
+printf("%s", GDrivers[driver]);
+
+setcolor(1);
+circle(100, 100, 50);
+
+setcolor(2);
+circle(200, 100, 50);
+
+setcolor(3);
+circle(300, 100, 50);
+
+setcolor(4);
+circle(400, 100, 50);
+
+getche();
+
+closegraph();
+
+}
